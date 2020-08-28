6663d69dac7a3167d9da044cd94cf47f392b3dbe
zhang2312600 <1602689751@qq.com>
2020-08-28 16:55:23 +0800

    Merge branch 'master' of https://github.com/zhang123690123/demoparent
                                           # Conflicts:
                                           #	demoparent/springbootEureka/pom.xml

--- demoparent/springbootService2/run.sh
+++ demoparent/springbootService2/run.sh
@@ -16,7 +16,7 @@ echo '----rm container----'
 docker rmi springbootservice2:1.0-SNAPSHOT
 echo '----rm image----      ${group_name}/${app_name}:${app_version}    '
 # 打包编译docker镜像
-docker build -t springbootservice2:1.0-SNAPSHOT . echo '----build image----'
+docker build -t springbootservice2:1.0-SNAPSHOT . \r\n echo '----build image----'
 docker run -p 8082:8082 --name springbootservice2
 -d springbootservice2:1.0-SNAPSHOT
 echo '----start container-----'
